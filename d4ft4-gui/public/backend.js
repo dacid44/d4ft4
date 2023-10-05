103a1e01d3f82bd279ce991eac50ca2aade1aaff
dacid44 <dacid44@gmail.com>
2023-10-05 17:23:34 -0500

    removed old IPC code

--- d4ft4-gui/public/backend.js
+++ d4ft4-gui/public/backend.js
@@ -1,68 +1,3 @@
-function returnQueue(callPort, returnPort) {
-    if (!(callPort in app.ports))
-        throw new Error(`Missing call port: ${callPort}`);
-    if (!(returnPort in app.ports))
-        throw new Error(`Missing return port: ${returnPort}`);
-
-    let nextCallId = 0;
-    let nextReturnId = 0;
-    let waitingResponses = new Map();
-
-    function nextId() {
-        return nextCallId++;
-    }
-
-    function returnWaiting() {
-        while (waitingResponses.has(nextReturnId)) {
-            app.ports[returnPort].send(waitingResponses.get(nextReturnId));
-            waitingResponses.delete(nextReturnId);
-            nextReturnId++;
-        }
-    }
-
-    function handleResponse(id, value) {
-        if (id == nextReturnId) {
-            app.ports[returnPort].send(value);
-            nextReturnId++;
-            returnWaiting();
-        } else {
-            waitingResponses.set(id, value);
-        }
-    }
-
-    return { nextId, handleResponse }
-}
-
-function addFunction(app, name, args, callPort, returnPort) {
-    let queue = returnQueue(callPort, returnPort);
-
-    app.ports[callPort].subscribe(value => {
-        const id = queue.nextId();
-        console.log("call", name, id, value);
-        invoke(name, args(value))
-            .then(value => {
-                console.log("response", name, id, value);
-                queue.handleResponse(id, value);
-            });
-    });
-}
-
-// returns a Result from the command
-function addFallibleFunction(app, name, args, callPort, returnPort) {
-    let queue = returnQueue(callPort, returnPort);
-
-    app.ports[callPort].subscribe(value => {
-        const id = queue.nextId();
-        invoke(name, args(value))
-            .then(value => {
-                queue.handleResponse(id, { Ok: value })
-            })
-            .catch(error => {
-                queue.handleResponse(id, { Err: error })
-            })
-    })
-}
-
 async function handleResponses(app) {
     while (true) {
         app.ports.receiveResponse.send(
@@ -72,13 +7,6 @@ async function handleResponses(app) {
 }
 
 function initBackend(app) {
-    // addFunction(app, "handle_message", (call) => ({ call }), "sendCall", "receiveResponse");
-
-    // app.ports.callSelectFile.subscribe(({ connId, save }) => {
-    //     (save ? dialog.save() : dialog.open({ directory: false, multiple: false }))
-    //         .then(path => app.ports.returnSelectFile.send([connId, path]));
-    // })
-    // addFunction(app, "open_file_dialog", (save) => ({ save }), "callOpenFileDialog", "returnOpenFileDialog");
     app.ports.sendCall.subscribe(call => invoke("handle_message", { call }));
     handleResponses(app);
 }
